66db39005c9c0dca0607cc5c777deee1981eda76
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-04-04 00:06:54 +0530

    Add: longest-substring-without-duplicates - submission-9

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-9.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int answer = 0, start =0;
+        Map<Character, Integer> chars = new HashMap<>();
+        for (int end=0; end<s.length(); end++) {
+            if (chars.containsKey(s.charAt(end))) {
+                start = Math.max(start, chars.get(s.charAt(end))+1);
+            }
+            chars.put(s.charAt(end), end);
+            answer = Math.max(answer, end-start+1);            
+        }
+        return answer;
+    }
+}
