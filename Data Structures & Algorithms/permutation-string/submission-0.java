da5b1612d0e02740f97b9984bf570f86f0727d81
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-04-09 00:35:25 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        String s1Str = Arrays.toString(getArr(s1));
+        // System.out.println(s1Str);
+        int l=0, r =l+s1.length();
+        while (l < r && r < s2.length()) {
+            String subStr = Arrays.toString(getArr(s2.substring(l, r)));
+            // System.out.println(subStr);
+            if (s1Str.equals(subStr)) {
+                return true;
+            }
+            l = r+1;
+            r = l+s1.length();
+        }
+        return false;
+    }
+
+    private int[] getArr(String s) {
+        // System.out.println(s);
+        int[] arr = new int[26];
+        for (char c1 : s.toCharArray()) {
+            arr[c1 -'a']++;
+        }
+        return arr;
+    }
+}
