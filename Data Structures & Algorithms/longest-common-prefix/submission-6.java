eba7a7a082a4cf7a307a438ca5f6b163e05f6c61
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-05-28 08:00:29 +0530

    Add: longest-common-prefix - submission-6

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-6.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i=0; i < prefix.length(); i++) {
+            for (String str : strs) {
+                if (str.length() == i+1 || prefix.charAt(i) != str.charAt(i)) {
+                    return str.substring(0, i);
+                }
+            }
+        }
+        return prefix;
+    }
+}
