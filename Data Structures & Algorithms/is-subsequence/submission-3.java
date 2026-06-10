ae3cf5403b96e3d7b959aab0daff3df81a485e70
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-06-10 08:30:46 +0530

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            if (i == s.length()) {
+                return true;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
