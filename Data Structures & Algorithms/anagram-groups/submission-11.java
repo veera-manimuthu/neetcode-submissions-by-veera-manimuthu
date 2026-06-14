e0db930c84954d2c7311aca58833cad248fb604d
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-06-14 08:17:57 +0530

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] arr = new int[26];
+            for (char ch : str.toCharArray()) {
+                arr[ch - 'a']++;
+            }
+            String key = Arrays.toString(arr);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
