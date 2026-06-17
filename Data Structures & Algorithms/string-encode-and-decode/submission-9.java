0541e9cfa55f4f9ed8a06b6bd6438c2df87a919a
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-06-17 08:05:55 +0530

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder strBldr = new StringBuilder();
+        int i = 0;
+        for (String str : strs) {
+            i++;
+            strBldr.append(str.length());
+            if (strs.size() == i) {
+                strBldr.append("$");
+            } else {
+                strBldr.append(",");
+            }
+        }
+        i = 0;
+        for (String str : strs) {
+            i++;
+            strBldr.append(str);
+            if (strs.size() != i) {
+                strBldr.append("#");
+            }
+        }
+        System.out.println(strBldr);
+        return strBldr.toString();
+    }
+
+    public List<String> decode(String str) {
+        System.out.println(str);
+        List<String> decoded = new ArrayList<>();
+        String[] strSplit = str.split("\\$");
+        List<String> lengths = Arrays.asList(strSplit[0].split("\\,"));
+        List<String> words = new ArrayList<>();
+        if (strSplit.length > 1) {
+            words = Arrays.asList(strSplit[1].split("\\#"));
+        }
+        int i = 0;
+        for (String len : lengths) {
+            int length = Integer.parseInt(len);
+            if (length == 0) {
+                decoded.add("");
+            } else if (length == 1) {
+                decoded.add("#");
+            } else if (words.size() > i && words.get(i).length() == length) {
+                decoded.add(words.get(i));
+            }
+            i++;
+        }
+        return decoded;
+    }
+}
