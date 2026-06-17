620403eb3e5bcec1aaa40786a9a5952502a2ec5e
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-06-17 07:53:22 +0530

    Add: majority-element-ii - submission-3

---
+++ Data Structures & Algorithms/majority-element-ii/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int elem1 = Integer.MIN_VALUE, elem2 = Integer.MIN_VALUE, cnt1 = 0, cnt2 = 0;
+        for (int num : nums) {
+            if (cnt1 == 0 || (elem1 == num && elem2 != num)) {
+                elem1 = num;
+                cnt1++;
+            } else if (cnt2 == 0 || (elem2 == num && elem1 != num)) {
+                elem2 = num;
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int num : nums) {
+            if (elem1 == num) {
+                cnt1++;
+            } else if (elem2 == num) {
+                cnt2++;
+            }
+        }
+
+        List<Integer> majority = new ArrayList<>();
+        if (cnt1 > nums.length / 3) {
+            majority.add(elem1);
+        }
+        if (cnt2 > nums.length / 3) {
+            majority.add(elem2);
+        }
+        return majority;
+    }
+}
