e41aea62c42bbf88e062b2df34ef428cfa880142
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-09-15 23:40:48 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (stack.isEmpty() && (ch == ')' || ch == '}' || ch == ']')) {
+                return false;
+            } else if (!stack.isEmpty()) {
+                if (ch == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (ch == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (ch == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
