6ea95d27a0de1c0daadc9f4f081353bcff4db8ec
Veerapandian Manimuthu <veera.manimuthu@outlook.com>
2026-09-19 23:09:43 +0530

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        List<String> operators = Arrays.asList("+", "-", "*", "/");
+        Stack<String> stack = new Stack<>();
+        int answer = 0;
+        for (int i = 0; i < tokens.length; i++) {
+            if (operators.contains(tokens[i])) {
+                int second = Integer.parseInt(stack.pop());
+                int first = Integer.parseInt(stack.pop());
+                switch (tokens[i]) {
+                    case "+":
+                        if (i + 1 == tokens.length) {
+                            answer = first + second;
+                        } else {
+                            stack.push(String.valueOf(first + second));
+                        }
+                        break;
+                    case "-":
+                        if (i + 1 == tokens.length) {
+                            answer = first - second;
+                        } else {
+                            stack.push(String.valueOf(first - second));
+                        }
+                        break;
+                    case "*":
+                        if (i + 1 == tokens.length) {
+                            answer = first * second;
+                        } else {
+                            stack.push(String.valueOf(first * second));
+                        }
+                        break;
+                    case "/":
+                        if (i + 1 == tokens.length) {
+                            answer = first / second;
+                        } else {
+                            stack.push(String.valueOf(first / second));
+                        }
+                        break;
+                }
+            } else {
+                stack.push(tokens[i]);
+            }
+        }
+        return answer;
+    }
+}
